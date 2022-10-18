d3d3d3462052c2353cecc93cfad1629245d8e69f
nathancurraneq <nathan.curran@everquote.com>
2022-10-18 15:16:25 +0100

    predictor updates

--- numSeqGen.py
+++ numSeqGen.py
@@ -1,6 +1,6 @@
 import csv
 
-array = [1, 1, 1, 1, 1, 1, 1]
+array = [142, 22, 31, 4, 15, 66, 97]
 
 # open the file in the write mode
 f = open('csvFile.csv', 'w')
@@ -12,7 +12,7 @@ writer = csv.writer(f)
 writer.writerow(array)
 for i in range(0, 49):
     for i in range(0, len(array)):
-        array[i] += 1
+        array[i] = array[i] + 10 - 2
     writer.writerow(array)
 
 f.close()
---
+++ numSeqPredictor.py
@@ -0,0 +1,35 @@
+import csv
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+
+def get_past_seqs():
+    f = open('csvFile.csv', 'r')
+    file_read = csv.reader(f)
+    array = list(file_read)
+    num_array = [list(map(int, i)) for i in array]
+    f.close()
+    return num_array
+
+
+past_num_seqs = get_past_seqs()
+print(past_num_seqs)
+
+x=[]
+for i in range(0, len(past_num_seqs)):
+    x.append([i])
+
+to_predict_x = [50, 51]
+to_predict_x = np.array(to_predict_x).reshape(-1, 1)
+print(to_predict_x)
+
+regsr = LinearRegression()
+regsr.fit(x, past_num_seqs)
+
+predicted_y = regsr.predict(to_predict_x)
+m = regsr.coef_
+c = regsr.intercept_
+print("Predicted y:\n", predicted_y)
+print("slope (m): ", m)
+print("y-intercept (c): ", c)
